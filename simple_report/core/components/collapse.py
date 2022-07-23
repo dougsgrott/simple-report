a3c9e872a0f4b2d24f7978f84e9f062591b8ef8c
neovelleitas <doug.sgrott@gmail.com>
2022-07-22 22:19:52 -0300

    Adding Collapse component and template.

---
+++ simple_report/core/components/collapse.py
@@ -0,0 +1,25 @@
+import uuid
+from jinja2 import Environment, FileSystemLoader
+
+from simple_report.core.elements.base import BaseElement
+
+env = Environment(loader=FileSystemLoader('simple_report/structure/html/templates'))
+
+
+class Collapse(BaseElement):
+    def __init__(self, use_panel, toggle_text, content, **kwargs):
+        self.use_panel = use_panel
+        self.toggle_text = toggle_text
+        self.content = content
+
+    def to_html(self):
+        toggle_btn_id = uuid.uuid4().hex[:10].upper()
+        content = {
+            'use_panel': self.use_panel,
+            'anchor_id': toggle_btn_id,
+            'toggle_text': self.toggle_text,
+            'content': self.content
+            }
+        template = env.get_template('collapse.html')
+        rendered_template = template.render(content)
+        return rendered_template
