edcfa9eb303b207a17645e92ded0a7ec47a32943
dougsgrott <doug.sgrott@gmail.com>
2022-07-26 08:22:37 -0300

    Adding Tabs component and template.

---
+++ simple_report/core/components/tabs.py
@@ -0,0 +1,25 @@
+import uuid
+from jinja2 import Environment, FileSystemLoader
+
+from simple_report.core.elements.base import BaseElement
+
+env = Environment(loader=FileSystemLoader('simple_report/structure/html/templates'))
+
+
+class Tabs(BaseElement):
+    def __init__(self, content, use_tabs, **kwargs):
+        super().__init__(**kwargs)
+        self.use_tabs = use_tabs
+        self.content = content
+
+    def to_html(self):
+        tabs_id = uuid.uuid4().hex[:10].upper()
+        content = {
+            'use_tabs': self.use_tabs,
+            'anchor_id': tabs_id,
+            'content': self.content,
+            'tabs_id': tabs_id,
+            }
+        template = env.get_template('tabs.html')
+        rendered_template = template.render(content, enumerate=enumerate)
+        return rendered_template
