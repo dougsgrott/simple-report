072d4bd191d09cf1007041f32be6b6530917450d
Doug <doug.sgrott@gmail.com>
2023-04-23 15:48:49 -0300

    Added basic table height configuration

--- simple_report/core/components/table.py
+++ simple_report/core/components/table.py
@@ -5,9 +5,11 @@ from simple_report.structure.html.templates import jinja2_env
 
 class Table(object):
 
-    def __init__(self, dataframe, title=None, use_striped_style=True, use_jquery=False, caption=None):
+    def __init__(self, dataframe, title=None, height_limit=None, use_striped_style=True, use_jquery=False, caption=None):
         self.dataframe = dataframe
         self.title = title
+        self.height = height_limit
+        self.use_height_limit = height_limit is not None
         self.use_striped_style = use_striped_style
         self.use_jquery = use_jquery
         self.caption = caption
@@ -39,11 +41,13 @@ class Table(object):
 
         content = {
             'title': self.title,
+            'height': self.height,
+            'use_height_limit': self.use_height_limit,
             'caption': self.caption,
             'table': table,
         }
         template = jinja2_env.get_template('table.html')
-        rendered_template = template.render(content)
+        rendered_template = template.render(content, none=None)
         return rendered_template
 
 
